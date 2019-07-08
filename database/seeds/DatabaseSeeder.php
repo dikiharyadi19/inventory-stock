2fd65b5e2f150bae88097def9c832ce9b68d8f18
root <root@localhost.localdomain>
2019-07-07 23:09:57 -0400

    larastock

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'email' =>'dk.harryadi@gmail.com',
+            'password' => bcrypt('admin'),
+        ]);
     }
 }
